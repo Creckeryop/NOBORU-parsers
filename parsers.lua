2ae2d51b5ced479dbb8bb7f9fab435465d28c5af
Creckeryop <didager@yandex.ru>
2019-12-30 00:50:22 +0500

    Update parsers.lua

--- parsers.lua
+++ parsers.lua
@@ -21,15 +21,3 @@ function ReadManga:getManga (i)
 	end
 	return list
 end
-
-function ReadManga:getChapters (manga)
-	local file = Net.downloadString(manga.link)
-	local list = {}
-	for link, name in file:gmatch("<td class%=.-<a href%=\""..manga.link.."(/vol%S-)\".->(.-)</a>") do
-		if link:match("^/") then
-			list[#list + 1] = {name = name:gsub("%s+"," "), link = link, pages = {}}
-			Console.addLine("got chapter\""+list[#list].name"\" ("..list[#list].link..")", LUA_COLOR_RED)
-		end
-	end
-	return list
-end
