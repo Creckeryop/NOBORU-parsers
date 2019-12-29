7e98e7012522949a4fb8a19b306ec44129811e31
Creckeryop <didager@yandex.ru>
2019-12-29 20:08:48 +0500

    Create parsers.lua

---
+++ parsers.lua
@@ -0,0 +1,23 @@
+MangaReader = Parser:new("MangaReader", "https://www.mangareader.net/", "ENG")
+
+function MangaReader:getManga (i)
+    local file = Net.downloadString ('https://www.mangareader.net/popular/'..(i - 1) * 30)
+    local list = {}
+	for img_link, link, name in file:gmatch ("image:url%('(%S-)'.-<div class=\"manga_name\">.-<a href=\"(%S-)\">(.-)</a>") do
+        list[#list + 1] = Manga:new (name, link, img_link, self)
+	end
+	return list
+end
+
+ReadManga = Parser:new("ReadManga", "https://readmanga.me/", "RUS")
+
+function ReadManga:getManga (i)
+	local file = Net.downloadString("http://readmanga.me/list?sortType=rate&offset="..((i - 1) * 70))
+	local list = {}
+	for link, img_link, name in file:gmatch("<a href=\"(/%S-)\" class=\"non%-hover\".-original='(%S-)' title='(.-)'") do
+		if link:match("^/") then
+			list[#list + 1] = Manga:new(name, link, img_link, self)
+		end
+	end
+	return list
+end
