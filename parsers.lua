03adefad63c9bf5135324b029318e494ef820eca
Creckeryop <didager@yandex.ru>
2020-01-17 20:13:09 +0500

    Updated ReadManga, MintManga, MangaPanda

--- parsers.lua
+++ parsers.lua
@@ -71,167 +71,81 @@ end
 
 ReadManga = Parser:new("ReadManga", "https://readmanga.me", "RUS", 2)
 
-function ReadManga:getManga(i, table, index)
+function ReadManga:getManga(page, table)
 	local file = {}
-	Net.downloadStringAsync("http://readmanga.me/list?sortType=rate&offset=" .. ((i - 1) * 70), file, "string")
+	threads.DownloadStringAsync(self.Link.."/list?sortType=rate&offset=" .. ((page - 1) * 70), file, "string", true)
 	while file.string == nil do
 		coroutine.yield(false)
 	end
-	for link, img_link, name in file.string:gmatch('<a href="(/%S-)" class="non%-hover".-original=\'(%S-)\' title=\'(.-)\' alt') do
-		if link:match("^/") then
-			table[index][#table[index] + 1] = Manga:new(name, link, img_link, self)
+	local t = table
+	for Link, ImageLink, Name in file.string:gmatch('<a href="(/%S-)" class="non%-hover".-original=\'(%S-)\' title=\'(.-)\' alt') do
+		if Link:match("^/") then
+			t[#t + 1] = CreateManga(Name, Link, ImageLink, self.ID, self.Link..Link)
 		end
 		coroutine.yield(true)
 	end
 end
 
-function ReadManga:getChapters(manga, index)
+function ReadManga:getChapters(manga, table)
 	local file = {}
-	Net.downloadStringAsync("http://readmanga.me" .. manga.link, file, "string")
+	threads.DownloadStringAsync(self.Link .. manga.Link, file, "string", true)
 	while file.string == nil do
 		coroutine.yield(false)
 	end
-	for link, name in file.string:gmatch('<td class%=.-<a href%="/.-(/vol%S-)".->(.-)</a>') do
-		local chapter = {name = name:gsub("%s+", " "), link = link, pages = {}, manga = manga}
-		manga[index][#manga[index] + 1] = chapter
+	local t = {}
+	for Link, Name in file.string:gmatch('<td class%=.-<a href%="/.-(/vol%S-)".->%s*(.-)</a>') do
+		t[#t + 1] = {
+			Name = Name:gsub("%s+", " "):gsub("<sup>.-</sup>",""):gsub("&quot;","\""):gsub("&amp;","&"):gsub("&#92;","\\"):gsub("&#39;","'"), 
+			Link = Link,
+			Pages = {},
+			Manga = manga
+		}
 	end
-	manga[index] = TableReverse(manga[index])
+	for i = #t, 1, -1 do
+		table[#table + 1] = t[i]
+	end
 end
 
-function ReadManga:getChapterInfo(chapter, index)
+function ReadManga:prepareChapter(chapter, table)
 	local file = {}
-	Net.downloadStringAsync("http://readmanga.me" .. chapter.manga.link .. chapter.link .. "?mtr=1", file, "string")
+	threads.DownloadStringAsync(self.Link .. chapter.Manga.Link .. chapter.Link .. "?mtr=1", file, "string", true)
 	while file.string == nil do
 		coroutine.yield(false)
 	end
 	local text = file.string:match("rm_h.init%( %[%[(.-)%]%]")
+	local t = table
 	if text ~= nil then
 		local list = load("return {{" .. text:gsub("%],%[", "},{") .. "}}")()
 		for i = 1, #list do
-			chapter[index][i] = list[i][2] .. list[i][3]
+			t[i] = list[i][2] .. list[i][3]
+			Console.writeLine("Got "..t[i])
 		end
 	end
 end
 
-function ReadManga:getMangaFromUrl(url)
+function ReadManga:loadChapterPage(link, table)
+	table.Link = link
+end
+
+function ReadManga:getMangaUrl(url, table)
 	local file = {}
-	Net.downloadStringAsync("http://www.readmanga.me" .. url, file, "string")
+	threads.DownloadStringAsync(self.Link .. url, file, "string", true)
 	while file.string == nil do
 		coroutine.yield(false)
 	end
-	local name = file.string:match("<span class%='name'>(.-)</span>")
-	local img_link = file.string:match('<img class="" src%="(.-)"')
-	return Manga:new(name, url, img_link, self)
-end
-
-MintManga = Parser:new("MintManga", "https://mintmanga.live", "RUS", 3)
-
-function MintManga:getManga(i, table, index)
-	local file = {}
-	Net.downloadStringAsync("http://mintmanga.live/list?sortType=rate&offset=" .. ((i - 1) * 70), file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	for link, img_link, name in file.string:gmatch('<a href="(/%S-)" class="non%-hover".-original=\'(%S-)\' title=\'(.-)\' alt') do
-		if link:match("^/") then
-			table[index][#table[index] + 1] = Manga:new(name, link, img_link, self)
-		end
-		coroutine.yield(true)
+	table.Name = file.string:match("<span class%='name'>(.-)</span>")
+	table.ImageLink = file.string:match('<img class="" src%="(.-)"')
+	table.Link = url
+	table.ParserID = self.ID
+	table.RawLink = self.Link .. url
+	if table.Name == nil or table.ImageLink == nil then
+		table.Name = nil
+		table.ImageLink = nil
+		table.Link = nil
+		table.ParserID = nil
+		table.RawLink = nil
 	end
 end
 
-function MintManga:getChapters(manga, index)
-	local file = {}
-	Net.downloadStringAsync("http://mintmanga.live" .. manga.link, file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	for link, name in file.string:gmatch('<td class%=.-<a href%="/.-(/vol%S-)".->(.-)</a>') do
-		local chapter = {name = name:gsub("%s+", " "), link = link, pages = {}, manga = manga}
-		manga[index][#manga[index] + 1] = chapter
-	end
-	manga[index] = TableReverse(manga[index])
-end
-
-function MintManga:getChapterInfo(chapter, index)
-	local file = {}
-	Net.downloadStringAsync("http://mintmanga.live" .. chapter.manga.link .. chapter.link .. "?mtr=1", file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	local text = file.string:match("rm_h.init%( %[%[(.-)%]%]")
-	if text ~= nil then
-		local list = load("return {{" .. text:gsub("%],%[", "},{") .. "}}")()
-		for i = 1, #list do
-			chapter[index][i] = list[i][2] .. list[i][3]
-		end
-	end
-end
-
-function MintManga:getMangaFromUrl(url)
-	local file = {}
-	Net.downloadStringAsync("http://www.mintmanga.live" .. url, file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	local name = file.string:match("<span class%='name'>(.-)</span>")
-	local img_link = file.string:match('<img class="" src%="(.-)"')
-	return Manga:new(name, url, img_link, self)
-end
-
-MangaPanda = Parser:new("MangaPanda", "https://www.mangapanda.com", "ENG", 4)
-
-function MangaPanda:getManga(i, table, index)
-	local file = {}
-	Net.downloadStringAsync("https://www.mangapanda.com/popular/" .. ((i - 1) * 30), file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	for img_link, link, name in file.string:gmatch('image:url%(\'(%S-)\'.-<div class="manga_name">.-<a href="(%S-)">(.-)</a>') do
-		table[index][#table[index] + 1] = Manga:new(name, link, img_link, self)
-		coroutine.yield(true)
-	end
-end
-
-function MangaPanda:getChapters(manga, index)
-	local file = {}
-	Net.downloadStringAsync("https://www.mangapanda.com" .. manga.link, file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	file.string = file.string:match('id="chapterlist"(.+)$') or ""
-	for link, name, subName in file.string:gmatch('<td>.-<a href%="/.-(/%S-)">(.-)</a>(.-)</td>') do
-		local chapter = {name = name .. subName, link = link, pages = {}, manga = manga}
-		manga[index][#manga[index] + 1] = chapter
-		--Console.addLine ("Parser: Got chapter \""..chapter.name.."\" ("..chapter.link..")", LUA_COLOR_GREEN)
-	end
-end
-
-function MangaPanda:getChapterInfo(chapter, index)
-	local file = {}
-	Net.downloadStringAsync("https://www.mangapanda.com" .. chapter.manga.link .. chapter.link .. "#", file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	local count = file.string:match(" of (.-)<")
-	for i = 1, count do
-		file = {}
-		Net.downloadStringAsync("https://www.mangapanda.com" .. chapter.manga.link .. chapter.link .. "/" .. i, file, "string")
-		while file.string == nil do
-			coroutine.yield(false)
-		end
-		chapter[index][i] = file.string:match('id="img".-src="(.-)"')
-		coroutine.yield(true)
-	end
-end
-
-function MangaPanda:getMangaFromUrl(url)
-	local file = {}
-	Net.downloadStringAsync("https://www.mangapanda.com" .. url, file, "string")
-	while file.string == nil do
-		coroutine.yield(false)
-	end
-	local name = file.string:match('aname">(.-)<')
-	local img_link = file.string:match('mangaimg">.-src%="(.-)"')
-	return Manga:new(name, url, img_link, self)
-end
+MintManga = ReadManga:new("MintManga", "https://mintmanga.live", "RUS", 3)
+MangaPanda = MangaReader:new("MangaPanda", "https://www.mangapanda.com", "ENG", 4)
