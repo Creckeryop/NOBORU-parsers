5d6d752fefa00418dbf31a8c2d7d61ea61c6d471
Creckeryop <didager@yandex.ru>
2020-01-20 21:55:43 +0500

    Added MangaHubParser and NSFW parameter

--- parsers.lua
+++ parsers.lua
@@ -151,6 +151,9 @@ end
 MintManga = ReadManga:new("MintManga", "https://mintmanga.live", "RUS", 3)
 MangaPanda = MangaReader:new("MangaPanda", "https://www.mangapanda.com", "ENG", 4)
 NineHentai = Parser:new("NineHentai", "https://9hentai.com","DIF",5)
+
+NineHentai.NSFW = true
+
 NineHentai.query = [[
 	{
 		"search":{
@@ -211,6 +214,8 @@ end
 
 NudeMoon = Parser:new("Nude-Moon", "https://nude-moon.net", "RUS", 6)
 
+NudeMoon.NSFW = true
+
 function NudeMoon:getManga(page, table)
 	local file = {}
 	threads.DownloadStringAsync(self.Link.."/all_manga?rowstart=" .. ((page - 1) * 30), file, "string", true)
@@ -250,5 +255,63 @@ function NudeMoon:prepareChapter(chapter, table)
 end
 
 function NudeMoon:loadChapterPage(link, table)
+	table.Link = link
+end
+
+MangaHub = Parser:new("MangaHub", "https://mangahub.io", "ENG", 7)
+
+function MangaHub:getManga(page, table)
+	local file = {}
+	threads.DownloadStringAsync(self.Link.."/popular/page/" .. page, file, "string", true)
+	while file.string == nil do
+		coroutine.yield(false)
+	end
+	local t = table
+	for Link, ImageLink, Name in file.string:gmatch("media%-left\">.-<a href=\"([^\"]-/manga/[^\"]-)\">.-src=\"([^\"]-)\" alt=\"(.-)\"") do
+		local manga = CreateManga(Name:gsub("&#x27;","'"), Link, ImageLink, self.ID, Link)
+		if manga then
+			t[#t + 1] = manga
+		end
+		coroutine.yield(true)
+	end
+end
+
+function MangaHub:getChapters(manga, table)
+	local file = {}
+	threads.DownloadStringAsync(manga.Link, file, "string", true)
+	while file.string == nil do
+		coroutine.yield(false)
+	end
+	local t = {}
+	for Link, Name in file.string:gmatch("<li.-<a href=\"([^\"]-/chapter.-chapter[^\"]-)\".-([^>]+)</span>") do
+		t[#t + 1] = {
+			Name = Name,
+			Link = Link,
+			Pages = {},
+			Manga = manga
+		}
+	end
+	for i = #t, 1, -1 do
+		table[#table + 1] = t[i]
+	end
+end
+MangaHub.query = [[
+		{"query":"{chapter(x:m01,slug:\"%s\",number:%s){id,title,mangaID,number,slug,date,pages,noAd,manga{id,title,slug,mainSlug,author,isWebtoon,isYaoi,isPorn,isSoftPorn,unauthFile,isLicensed}}}"}
+]]
+function MangaHub:prepareChapter(chapter, table)
+	local file = {}
+	threads.DownloadStringAsync("https://api2.mangahub.io/graphql", file, "string", true, POST_METHOD, string.format(MangaHub.query,chapter.Link:match(".-/chapter/(.-)/chapter%-([^/]+)")),JSON)
+	while file.string == nil do
+		coroutine.yield(false)
+	end
+	local t = table
+	local pages = file.string:match("\"pages\":\"{(.-)}\",\"noAd\"")
+	for link in pages:gmatch(":\\\"(.-)\\\"") do
+		t[#t + 1] = "https://cdn.mangahub.io/file/imghub/"..link
+		Console.writeLine("Got "..t[#t])
+	end
+end
+
+function MangaHub:loadChapterPage(link, table)
 	table.Link = link
 end
