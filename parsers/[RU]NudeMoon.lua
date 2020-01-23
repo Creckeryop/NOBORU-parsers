cfbfbbaf6d03d721cfbe4ce736f60b252bdd2f7d
Creckeryop <didager@yandex.ru>
2020-01-23 13:33:31 +0500

    Added search for NudeMoon

--- parsers/[RU]NudeMoon.lua
+++ parsers/[RU]NudeMoon.lua
@@ -33,7 +33,31 @@ function NudeMoon:getPopularManga(page, table)
 		coroutine.yield(false)
 	end
 end
-
+local concat = table.concat
+function NudeMoon:searchManga(data, page, table)
+	local file = {}
+	local stext = {}
+	for c in it_utf8(data) do
+		if utf8ascii[c] then
+			stext[#stext+1] = utf8ascii[c]
+		else
+			stext[#stext+1] = c
+		end
+	end
+	stext = concat(stext):gsub("%%","%%%%")
+	Threads.DownloadStringAsync(self.Link..string.format("/search?stext=%s&rowstart=%s",stext,((page - 1) * 30)), file, "string", true)
+	while file.string == nil do
+		coroutine.yield(false)
+	end
+	local t = table
+	for Link, Name, ImageLink in file.string:gmatch("<td colspan.-<a href=\"(.-)\".-title=\"(.-)\".-src=\"(.-)\"") do 
+		local manga = CreateManga(AnsiToUtf8(Name), Link, self.Link..ImageLink, self.ID, self.Link..Link)
+		if manga then
+			t[#t + 1] = manga
+		end
+		coroutine.yield(false)
+	end
+end
 
 function NudeMoon:getChapters(manga, table)
 	table[#table + 1] = {
