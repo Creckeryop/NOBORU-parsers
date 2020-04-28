9db1b2ca670e2f355833a21b5767bb695a3ebd6a
Creckeryop <didager@yandex.ru>
2020-04-28 17:49:42 +0500

    [READCOMICSONLINE] Code Refactoring + HTML entities

--- parsers/[EN]ReadComicsOnline.lua
+++ parsers/[EN]ReadComicsOnline.lua
@@ -1,90 +1,73 @@
 --https://readcomicsonline.ru/filterList?sortBy=Views&page=1
-ReadComicsOnline = Parser:new("ReadComicsOnline", "https://readcomicsonline.ru", "ENG", "READCONLINEENG")
+ReadComicsOnline = Parser:new("ReadComicsOnline", "https://readcomicsonline.ru", "ENG", "READCONLINEENG", 1)
 
-function ReadComicsOnline:getManga(link, dest_table)
-    local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
+local function stringify(string)
+    return string:gsub("&#([^;]-);", function(a)
+        local number = tonumber("0" .. a) or tonumber(a)
+        return number and u8c(number) or "&#" .. a .. ";"
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
+end
+
+local function downloadContent(link)
+    local f = {}
+    Threads.insertTask(f, {
+        Type = "StringRequest",
+        Link = link,
+        Table = f,
+        Index = "text"
+    })
+    while Threads.check(f) do
+        coroutine.yield(false)
     end
-    local content = file.string or ""
-    local t = dest_table
-    local done = true
+    return f.text or ""
+end
+
+function ReadComicsOnline:getManga(link, dt)
+    local content = downloadContent(link)
+	dt.NoPages = true
 	for Link, ImageLink, Name in content:gmatch("<a href=\"([^\"]-)\">[^>]-src='//([^']-)' alt='([^']-)'>[^<]-</a>") do
-		local manga = CreateManga(Name, Link, ImageLink, self.ID, Link)
-		if manga then
-			t[#t + 1] = manga
-			done = false
-		end
+		dt[#dt + 1] = CreateManga(stringify(Name), Link, ImageLink, self.ID, Link)
+		dt.NoPages = false
 		coroutine.yield(false)
 	end
-	if done then
-		t.NoPages = true
-	end
 end
 
-function ReadComicsOnline:getLatestManga(page, dest_table)
-    self:getManga(self.Link.."/filterList?sortBy=last_release&asc=false&page="..page, dest_table)
+function ReadComicsOnline:getLatestManga(page, dt)
+    self:getManga(self.Link.."/filterList?sortBy=last_release&asc=false&page="..page, dt)
 end
 
-function ReadComicsOnline:getPopularManga(page, dest_table)
-    self:getManga(self.Link.."/filterList?sortBy=views&asc=false&page="..page, dest_table)
+function ReadComicsOnline:getPopularManga(page, dt)
+    self:getManga(self.Link.."/filterList?sortBy=views&asc=false&page="..page, dt)
 end
 
-function ReadComicsOnline:searchManga(search, page, dest_table)
-    self:getManga(self.Link.."/filterList?alpha="..search.."&sortBy=views&asc=false&page="..page, dest_table)
+function ReadComicsOnline:searchManga(search, page, dt)
+    self:getManga(self.Link.."/filterList?alpha="..search.."&sortBy=views&asc=false&page="..page, dt)
 end
 
-function ReadComicsOnline:getChapters(manga, dest_table)
-	local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = manga.Link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
-    end
-    local content = file.string or ""
+function ReadComicsOnline:getChapters(manga, dt)
+    local content = downloadContent(manga.Link)
     local t = {}
     for Link, Name in content:gmatch("chapter%-title%-rtl\">[^<]-<a href=\"([^\"]-)\">([^<]-)</a>") do
         t[#t + 1] = {
-			Name = Name,
+			Name = stringify(Name),
 			Link = Link,
 			Pages = {},
 			Manga = manga
 		}
     end
 	for i = #t, 1, -1 do
-		dest_table[#dest_table + 1] = t[i]
+		dt[#dt + 1] = t[i]
 	end
 end
 
-function ReadComicsOnline:prepareChapter(chapter, dest_table)
-	local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = chapter.Link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
-    end
-    local content = file.string or ""
-	local t = dest_table
+function ReadComicsOnline:prepareChapter(chapter, dt)
+    local content = downloadContent(chapter.Link)
 	for Link in content:gmatch("img%-responsive\"[^>]-data%-src=' ([^']-) '") do
-        t[#t + 1] = Link
-		Console.write("Got " .. t[#t])
+        dt[#dt + 1] = Link
+		Console.write("Got " .. dt[#dt])
     end
 end
 
-function ReadComicsOnline:loadChapterPage(link, dest_table)
-	dest_table.Link = link
+function ReadComicsOnline:loadChapterPage(link, dt)
+	dt.Link = link
 end
