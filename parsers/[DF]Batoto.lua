d80b0d6219f1d98d60d5b00b04587f8f81846358
Creckeryop <didager@yandex.ru>
2020-04-29 22:59:58 +0500

    [BATOTO] Refactoring Code + HTML entities

--- parsers/[DF]Batoto.lua
+++ parsers/[DF]Batoto.lua
@@ -1,137 +1,130 @@
-if u8c then
-    BatoTo = Parser:new("Bato.TO", "https://bato.to", "DIF", "BATODIF", 1)
-    
-    local function stringify(string)
-        return string:gsub("&#([^;]-);", function(a)
-            local number = tonumber("0" .. a) or tonumber(a)
-            return number and u8c(number) or "&#" .. a .. ";"
-        end)
+BatoTo = Parser:new("Bato.TO", "https://bato.to", "DIF", "BATODIF", 2)
+
+local function stringify(string)
+    return string:gsub("&#([^;]-);", function(a)
+        local number = tonumber("0" .. a) or tonumber(a)
+        return number and u8c(number) or "&#" .. a .. ";"
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
+end
+
+local function downloadContent(link)
+    local f = {}
+    Threads.insertTask(f, {
+        Type = "StringRequest",
+        Link = link,
+        Table = f,
+        Index = "text"
+    })
+    while Threads.check(f) do
+        coroutine.yield(false)
     end
-    
-    local function downloadContent(link)
-        local file = {}
-        Threads.insertTask(file, {
-            Type = "StringRequest",
-            Link = link,
-            Table = file,
-            Index = "string"
-        })
-        while Threads.check(file) do
-            coroutine.yield(false)
-        end
-        return file.string or ""
+    return f.text or ""
+end
+
+local langs = {
+    RUS = "russian",
+    ENG = "english",
+    BRA = "brazilian",
+    FRA = "french",
+    POL = "polish",
+    DEU = "german",
+    SPA = "spanish",
+    TUR = "turkish",
+    ITA = "italian",
+    VIE = "vietnamese",
+    PRT = "portuguese"
+}
+
+local cntrys = {
+    russia = "RUS",
+    england = "ENG",
+    mexico = "MEX",
+    brazil = "BRA",
+    france = "FRA",
+    poland = "POL",
+    spain = "SPA",
+    germany = "DEU",
+    turkey = "TUR",
+    italy = "ITA",
+    vietnam = "VIE",
+    portugal = "PRT"
+}
+
+
+function BatoTo:getManga(link, dt)
+    local content
+    if link:find("latest") then
+        content = downloadContent(link):match('id="series%-list"(.-)class="footer') or ""
+    else
+        content = downloadContent(link):match('id="series%-list"(.-)class="browse%-pager"') or ""
     end
-    
-    local langs = {
-        RUS = "russian",
-        ENG = "english",
-        BRA = "brazilian",
-        FRA = "french",
-        POL = "polish",
-        DEU = "german",
-        SPA = "spanish",
-        TUR = "turkish",
-        ITA = "italian",
-        VIE = "vietnamese",
-        PRT = "portuguese"
-    }
-
-    local cntrys = {
-        russia = "RUS",
-        england = "ENG",
-        mexico = "MEX",
-        brazil = "BRA",
-        france = "FRA",
-        poland = "POL",
-        spain = "SPA",
-        germany = "DEU",
-        turkey = "TUR",
-        italy = "ITA",
-        vietnam = "VIE",
-        portugal = "PRT"
-    }
-
-
-    function BatoTo:getManga(link, dest_table)
-        local content
-        if link:find("latest") then
-            content = downloadContent(link):match('id="series%-list"(.-)class="footer') or ""
-        else
-            content = downloadContent(link):match('id="series%-list"(.-)class="browse%-pager"') or ""
+    dt.NoPages = true
+    for block in content:gmatch(' item (.-)class="col%-24') do
+        local flag = not block:find("no%-flag")
+        local cntry = "england"
+        if flag then
+            cntry = block:match('flag_(.-)"')
         end
-        local t = dest_table
-        local done = true
-        for block in content:gmatch(' item (.-)class="col%-24') do
-            local flag = not block:find("no%-flag")
-            local cntry = "england"
-            if flag then
-                cntry = block:match('flag_(.-)"')
+        ImageLink, Link, Name = block:match('cover.-src="//([^"]-)".-item%-text.-href="([^"]-)"[^>]->(.-)</a>')
+        Name = Name:gsub("<.->", "")
+        local manga = CreateManga(stringify(Name), Link, ImageLink, self.ID, self.Link .. Link)
+        if manga and (cntrys[cntry] == Settings.ParserLanguage or Settings.ParserLanguage == "DIF") then
+            if Settings.ParserLanguage == "DIF" then
+                manga.Name = "[" .. (cntrys[cntry] or (cntry:sub(0, 3):upper())) .. "] " .. manga.Name
             end
-            ImageLink, Link, Name = block:match('cover.-src="//([^"]-)".-item%-text.-href="([^"]-)"[^>]->(.-)</a>')
-            Name = Name:gsub("<.->","")
-            local manga = CreateManga(stringify(Name), Link, ImageLink, self.ID, self.Link .. Link)
-            if manga and (cntrys[cntry] == Settings.ParserLanguage or Settings.ParserLanguage == "DIF") then
-                if Settings.ParserLanguage == "DIF" then
-                    manga.Name = "["..(cntrys[cntry] or (cntry:sub(0, 3):upper())).."] "..manga.Name
-                end
-                t[#t + 1] = manga
-            end
-            done = false
-            coroutine.yield(false)
+            dt[#dt + 1] = manga
         end
-        if done then
-            t.NoPages = true
-        end
+        dt.NoPages = false
+        coroutine.yield(false)
     end
+end
 
-    function BatoTo:getLatestManga(page, dest_table)
-        local addition = ""
-        if Settings.ParserLanguage ~= "DIF" and langs[Settings.ParserLanguage] then
-            addition = "&langs="..langs[Settings.ParserLanguage]
-        end
-        self:getManga(self.Link .. "/latest?page=" .. page..addition,dest_table)
+function BatoTo:getLatestManga(page, dt)
+    local addition = ""
+    if Settings.ParserLanguage ~= "DIF" and langs[Settings.ParserLanguage] then
+        addition = "&langs=" .. langs[Settings.ParserLanguage]
     end
-    
-    function BatoTo:getPopularManga(page, dest_table)
-        local addition = ""
-        if Settings.ParserLanguage ~= "DIF" and langs[Settings.ParserLanguage] then
-            addition = "&langs="..langs[Settings.ParserLanguage]
-        end
-        self:getManga(self.Link .. "/browse?page=" .. page..addition,dest_table)
-    end
-    
-    function BatoTo:searchManga(search, page, dest_table)
-        self:getManga(self.Link .. "/search?q="..search.."&p="..page,dest_table)
-    end
+    self:getManga(self.Link .. "/latest?page=" .. page .. addition, dt)
+end
 
-    function BatoTo:getChapters(manga, dest_table)
-        local content = downloadContent(self.Link..manga.Link)
-        local t = {}
-        for Link, Name in content:gmatch('chapt" href="([^"]-)">.-<b>(.-)</a>') do
-            Name = Name:gsub("\n%s*(.-)\n%s*"," %1")
-            Name = stringify(Name):gsub("<.->","")
-            t[#t+1] = {
-                Name = Name:gsub(" %- Read Online",""),
-				Link = Link,
-				Pages = {},
-				Manga = manga
-            }
-        end
-        for i = #t, 1, -1 do
-            dest_table[#dest_table + 1] = t[i]
-        end
+function BatoTo:getPopularManga(page, dt)
+    local addition = ""
+    if Settings.ParserLanguage ~= "DIF" and langs[Settings.ParserLanguage] then
+        addition = "&langs=" .. langs[Settings.ParserLanguage]
     end
-    
-    function BatoTo:prepareChapter(chapter, dest_table)
-        local content = downloadContent(self.Link .. chapter.Link)
-        local t = dest_table
-        for Link in content:gmatch('"%d-":"([^"]-)"') do
-            t[#t + 1] = Link
-            Console.write("Got " .. t[#t])
-        end
-    end
+    self:getManga(self.Link .. "/browse?page=" .. page .. addition, dt)
+end
 
-    function BatoTo:loadChapterPage(link, dest_table)
-        dest_table.Link = link
+function BatoTo:searchManga(search, page, dt)
+    self:getManga(self.Link .. "/search?q=" .. search .. "&p=" .. page, dt)
+end
+
+function BatoTo:getChapters(manga, dt)
+    local content = downloadContent(self.Link .. manga.Link)
+    local t = {}
+    for Link, Name in content:gmatch('chapt" href="([^"]-)">.-<b>(.-)</a>') do
+        Name = Name:gsub("\n%s*(.-)\n%s*", " %1")
+        Name = stringify(Name):gsub("<.->", "")
+        t[#t + 1] = {
+            Name = Name:gsub(" %- Read Online", ""),
+            Link = Link,
+            Pages = {},
+            Manga = manga
+        }
     end
+    for i = #t, 1, -1 do
+        dt[#dt + 1] = t[i]
+    end
+end
+
+function BatoTo:prepareChapter(chapter, dt)
+    local content = downloadContent(self.Link .. chapter.Link)
+    for Link in content:gmatch('"%d-":"([^"]-)"') do
+        dt[#dt + 1] = Link
+        Console.write("Got " .. dt[#dt])
+    end
+end
+
+function BatoTo:loadChapterPage(link, dt)
+    dt.Link = link
 end
