a860bb5560a934312b259f4aa6b9654e4d2d8993
Creckeryop <didager@yandex.ru>
2020-04-29 22:54:36 +0500

    [MABUSHIMAJO] Refactoring Code + HTML entities

--- parsers/[TR]Mabushimajo.lua
+++ parsers/[TR]Mabushimajo.lua
@@ -1,94 +1,63 @@
-Mabushimajo = Parser:new("Mabushimajo","http://mabushimajo.com", "TUR", "MABUSHTR")
-
-local notify = false
+Mabushimajo = Parser:new("Mabushimajo", "http://mabushimajo.com", "TUR", "MABUSHTR", 1)
 
 local function stringify(string)
-    if not u8c then
-        if not notify then
-            Notifications.push("Please update app, to see fixed titles")
-            notify = true
-        end
-        return string
-    end
     return string:gsub("&#([^;]-);", function(a)
         local number = tonumber("0" .. a) or tonumber(a)
         return number and u8c(number) or "&#" .. a .. ";"
-    end)
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
 end
 
 local function downloadContent(link)
-    local file = {}
-    Threads.insertTask(file, {
+    local f = {}
+    Threads.insertTask(f, {
         Type = "StringRequest",
         Link = link,
-        Table = file,
-        Index = "string"
+        Table = f,
+        Index = "text"
     })
-    while Threads.check(file) do
+    while Threads.check(f) do
         coroutine.yield(false)
     end
-    return file.string or ""
+    return f.text or ""
 end
 
-function Mabushimajo:getManga(link, dest_table)
+function Mabushimajo:getManga(link, dt)
     local content = downloadContent(link)
-    local t = dest_table
-    for Link, ImageLink,Name in content:gmatch('class="group"><a href="(%S-)">.-src="(%S-)".-title=.->([^<]-)<') do
-		local manga = CreateManga(stringify(Name), Link:match("^.+/(.-)/$"), ImageLink, self.ID, Link)
-		if manga then
-			t[#t + 1] = manga
-		end
-		coroutine.yield(false)
-	end
-    t.NoPages = not content:find("Last")
-end
-
-function Mabushimajo:getPopularManga(page, dest_table)
-    self:getManga(self.Link.."/onlineokuma/directory/"..page, dest_table)
-end
-
-if not u8c then
-    function Mabushimajo:searchManga(search, page, dest_table)
-        Notifications.push("Parser don't support search feature")
-        Notifications.push("Please update App")
+    for Link, ImageLink, Name in content:gmatch('class="group"><a href="(%S-)">.-src="(%S-)".-title=.->([^<]-)<') do
+        dt[#dt + 1] = CreateManga(stringify(Name), Link:match("^.+/(.-)/$"), ImageLink, self.ID, Link)
+        coroutine.yield(false)
     end
+    dt.NoPages = not content:find("Last")
 end
 
-function Mabushimajo:getChapters(manga, dest_table)
+function Mabushimajo:getPopularManga(page, dt)
+    self:getManga(self.Link .. "/onlineokuma/directory/" .. page, dt)
+end
+
+function Mabushimajo:getChapters(manga, dt)
     local content = downloadContent(manga.RawLink)
     local t = {}
     for Link, Name in content:gmatch('class="element">.-<a href="(%S-)" title=[^>]->([^<]-)<') do
         t[#t + 1] = {
-			Name = Name,
-			Link = Link:match("read/(.+)"),
-			Pages = {},
-			Manga = manga
-		}
+            Name = stringify(Name),
+            Link = Link:match("read/(.+)"),
+            Pages = {},
+            Manga = manga
+        }
     end
-	for i = #t, 1, -1 do
-		dest_table[#dest_table + 1] = t[i]
-	end
+    for i = #t, 1, -1 do
+        dt[#dt + 1] = t[i]
+    end
 end
 
-function Mabushimajo:prepareChapter(chapter, dest_table)
-	local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = self.Link.."/onlineokuma/read/"..chapter.Link.."/page/1",
-		Table = file,
-        Index = "string",
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
-    end
-    local content = file.string or ""
-    local t = dest_table
+function Mabushimajo:prepareChapter(chapter, dt)
+    local content = downloadContent(self.Link .. "/onlineokuma/read/" .. chapter.Link .. "/page/1")
     for Link in content:gmatch('"url" ?: ?"(%S-)"') do
-        t[#t + 1] = Link:gsub("\\/","/")
-		Console.write("Got " .. t[#t])
+        dt[#dt + 1] = Link:gsub("\\/", "/")
+        Console.write("Got " .. dt[#dt])
     end
 end
 
-function Mabushimajo:loadChapterPage(link, dest_table)
-	dest_table.Link = link
+function Mabushimajo:loadChapterPage(link, dt)
+    dt.Link = link
 end
