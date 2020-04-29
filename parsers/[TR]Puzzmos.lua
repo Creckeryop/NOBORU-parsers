46b12f1be4b40c42bd2277ad48162cd85a8de66b
Creckeryop <didager@yandex.ru>
2020-04-29 19:43:42 +0500

    [PUZZMOS] Refactoring Code + HTML entities

--- parsers/[TR]Puzzmos.lua
+++ parsers/[TR]Puzzmos.lua
@@ -1,86 +1,62 @@
-Puzzmos = Parser:new("Puzzmos", "https://www.puzzmos.com", "TUR", "PUZZMOSTUR", 1)
+Puzzmos = Parser:new("Puzzmos", "https://www.puzzmos.com", "TUR", "PUZZMOSTUR", 2)
 
 local function downloadContent(link)
-    local file = {}
-    Threads.insertTask(file, {
+    local f = {}
+    Threads.insertTask(f, {
         Type = "StringRequest",
         Link = link,
-        Table = file,
-        Index = "string"
+        Table = f,
+        Index = "text"
     })
-    while Threads.check(file) do
+    while Threads.check(f) do
         coroutine.yield(false)
     end
-    return file.string or ""
+    return f.text or ""
 end
 
-local notify = false
-
 local function stringify(string)
-    if not u8c then
-        if not notify then
-            Notifications.push("Please update app, to see fixed titles")
-            notify = true
-        end
-        return string
-    end
     return string:gsub("&#([^;]-);", function(a)
         local number = tonumber("0" .. a) or tonumber(a)
         return number and u8c(number) or "&#" .. a .. ";"
-    end)
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
 end
 
 local function stringify2(string)
-    if not u8c then
-        if not notify then
-            Notifications.push("Please update app, to see fixed titles")
-            notify = true
-        end
-        return string
+    return string:gsub("\\u(....)", function(a) return u8c(tonumber("0x" .. a)) end)
+end
+
+function Puzzmos:getPopularManga(page, dt)
+    local content = downloadContent(self.Link .. "/filterList?sortBy=views&asc=false&page=" .. page)
+    dt.NoPages = true
+    for Link, ImageLink, Name in content:gmatch("<a href=\"([^\"]-)\" class=\"thumbnail\">[^>]-src='([^']-)' alt='([^']-)'>[^<]-</a>") do
+        dt[#dt + 1] = CreateManga(stringify(Name), Link:gsub("%%", "%%%%"), ImageLink:gsub(" ", "%%20"):gsub("%%", "%%%%"), self.ID, Link)
+        dt.NoPages = false
+        coroutine.yield(false)
     end
-    return string:gsub("\\u(....)",function(a) return u8c(tonumber(string.format("0x%s",a))) end)
 end
 
-function Puzzmos:getManga(link, dest_table)
-    local content = downloadContent(link)
-    local t = dest_table
-    local done = true
-	for Link, ImageLink, Name in content:gmatch("<a href=\"([^\"]-)\" class=\"thumbnail\">[^>]-src='([^']-)' alt='([^']-)'>[^<]-</a>") do
-		local manga = CreateManga(stringify(Name), Link:gsub("%%","%%%%"), ImageLink:gsub(" ","%%20"):gsub("%%","%%%%"), self.ID, Link)
-		if manga then
-			t[#t + 1] = manga
-			done = false
-		end
-		coroutine.yield(false)
-	end
-	if done then
-		t.NoPages = true
-	end
-end
-
-function Puzzmos:getPopularManga(page, dest_table)
-    self:getManga(self.Link.."/filterList?sortBy=views&asc=false&page="..page, dest_table)
-end
-
-function Puzzmos:getLatestManga(page, dest_table)
-    local content = downloadContent(self.Link.."/latest-release?page="..page, dest_table)
-    local t = dest_table
-    local done = true
+local unique = {}
+function Puzzmos:getLatestManga(page, dt)
+    local content = downloadContent(self.Link .. "/latest-release?page=" .. page, dt)
+    dt.NoPages = true
+    if page == 1 then
+        unique = {}
+    end
     for Link, Name in content:gmatch('"manga%-item">.-href="(%S-)">([^<]-)</a>') do
+        dt.NoPages = false
         local l = Link:match("/([^/]-)$") or ""
-		local manga = CreateManga(stringify(Name), self.Link.."/manga/"..l:gsub(" ","%%20"):gsub("%%","%%%%"), self.Link.."//uploads/manga/"..l:gsub("%%","%%%%").."/cover/cover_250x350.jpg", self.ID, Link)
-		if manga then
-			t[#t + 1] = manga
-			done = false
-		end
-		coroutine.yield(false)
-	end
-	if done then
-		t.NoPages = true
-	end
+        if not unique[Link] then
+            local manga = CreateManga(stringify(Name), self.Link .. "/manga/" .. l:gsub(" ", "%%20"):gsub("%%", "%%%%"), self.Link .. "//uploads/manga/" .. l:gsub("%%", "%%%%") .. "/cover/cover_250x350.jpg", self.ID, Link)
+            dt[#dt + 1] = manga
+            if manga then
+                unique[Link] = true
+            end
+            coroutine.yield(false)
+        end
+    end
 end
 
-function Puzzmos:searchManga(search, page, dest_table)
+function Puzzmos:searchManga(search, page, dt)
     local old_gsub = string.gsub
     string.gsub = function(self, one, sec)
         return old_gsub(self, sec, one)
@@ -88,42 +64,40 @@ function Puzzmos:searchManga(search, page, dest_table)
     search = search:gsub("!", "%%%%21"):gsub("#", "%%%%23"):gsub("%$", "%%%%24"):gsub("&", "%%%%26"):gsub("'", "%%%%27"):gsub("%(", "%%%%28"):gsub("%)", "%%%%29"):gsub("%*", "%%%%2A"):gsub("%+", "%%%%2B"):gsub(",", "%%%%2C"):gsub("%.", "%%%%2E"):gsub("/", "%%%%2F"):gsub(" ", "%+"):gsub("%%", "%%%%25")
     Console.write(search)
     string.gsub = old_gsub
-    local searchLink = self.Link.."/search?query="..search
+    local searchLink = self.Link .. "/search?query=" .. search
     local content = downloadContent(searchLink)
-    local t = dest_table
     for Name, Link in content:gmatch('"value":"([^"]-)","data":"([^"]-)"') do
-		local manga = CreateManga(stringify2(Name), self.Link.."/manga/"..stringify2(Link):gsub(" ","%%20"):gsub("%%","%%%%"), self.Link.."//uploads/manga/"..stringify2(Link):gsub(" ","%%20"):gsub("%%","%%%%").."/cover/cover_250x350.jpg", self.ID, self.Link.."/manga/"..stringify2(Link))
-        t[#t + 1] = manga
-		coroutine.yield(false)
-	end
-    t.NoPages = true
+        local manga = CreateManga(stringify2(Name), self.Link .. "/manga/" .. stringify2(Link):gsub(" ", "%%20"):gsub("%%", "%%%%"), self.Link .. "//uploads/manga/" .. stringify2(Link):gsub(" ", "%%20"):gsub("%%", "%%%%") .. "/cover/cover_250x350.jpg", self.ID, self.Link .. "/manga/" .. stringify2(Link))
+        dt[#dt + 1] = manga
+        coroutine.yield(false)
+    end
+    dt.NoPages = true
 end
 
-function Puzzmos:getChapters(manga, dest_table)
+function Puzzmos:getChapters(manga, dt)
     local content = downloadContent(manga.Link)
     local t = {}
     for Link, Name in content:gmatch("chapter%-title%-rtl\">[^<]-<a href=\"([^\"]-)\">([^<]-)</a>") do
         t[#t + 1] = {
-			Name = stringify(Name),
-			Link = Link:gsub(" ","%%20"):gsub("%%","%%%%"),
-			Pages = {},
-			Manga = manga
-		}
+            Name = stringify(Name),
+            Link = Link:gsub(" ", "%%20"):gsub("%%", "%%%%"),
+            Pages = {},
+            Manga = manga
+        }
     end
-	for i = #t, 1, -1 do
-		dest_table[#dest_table + 1] = t[i]
-	end
+    for i = #t, 1, -1 do
+        dt[#dt + 1] = t[i]
+    end
 end
 
-function Puzzmos:prepareChapter(chapter, dest_table)
+function Puzzmos:prepareChapter(chapter, dt)
     local content = downloadContent(chapter.Link)
-	local t = dest_table
-	for Link in content:gmatch("img%-responsive\"[^>]-data%-src=' ([^']-) '") do
-        t[#t + 1] = Link:gsub(" ","%%20"):gsub("%%","%%%%")
-		Console.write("Got " .. t[#t])
+    for Link in content:gmatch("img%-responsive\"[^>]-data%-src=' ([^']-) '") do
+        dt[#dt + 1] = Link:gsub(" ", "%%20"):gsub("%%", "%%%%")
+        Console.write("Got " .. dt[#dt])
     end
 end
 
-function Puzzmos:loadChapterPage(link, dest_table)
-	dest_table.Link = link
+function Puzzmos:loadChapterPage(link, dt)
+    dt.Link = link
 end
