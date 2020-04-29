0dcc71cf6e718d29d5572e90c6299a8905b6166e
Creckeryop <didager@yandex.ru>
2020-04-29 22:48:32 +0500

    [LEOMANGA] Refactoring Code + HTML entities

--- parsers/[ES]LeoManga.lua
+++ parsers/[ES]LeoManga.lua
@@ -1,85 +1,68 @@
-LeoManga = Parser:new("LeoManga", "https://leomanga.me", "ESP", "LEOMANGASPA")
+LeoManga = Parser:new("LeoManga", "https://leomanga.me", "ESP", "LEOMANGASPA", 1)
 
-function LeoManga:getManga(link, dest_table)
-    local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
+local function stringify(string)
+    return string:gsub("&#([^;]-);", function(a)
+        local number = tonumber("0" .. a) or tonumber(a)
+        return number and u8c(number) or "&#" .. a .. ";"
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
+end
+
+local function downloadContent(link)
+    local f = {}
+    Threads.insertTask(f, {
+        Type = "StringRequest",
+        Link = link,
+        Table = f,
+        Index = "text"
+    })
+    while Threads.check(f) do
+        coroutine.yield(false)
     end
-    local content = file.string or ""
-    local t = dest_table
-    local done = true
-	for Link, ImageLink, Name in content:gmatch("<a href=\"([^\"]-)\">[^>]-src='([^']-)' alt='([^']-)'>[^<]-</a>") do
-		local manga = CreateManga(Name, Link, ImageLink, self.ID, Link)
-		if manga then
-			t[#t + 1] = manga
-			done = false
-		end
-		coroutine.yield(false)
-	end
-	if done then
-		t.NoPages = true
-	end
+    return f.text or ""
 end
 
-function LeoManga:getPopularManga(page, dest_table)
-    self:getManga(self.Link.."/filterList?sortBy=views&asc=false&page="..page, dest_table)
-end
-
-function LeoManga:searchManga(search, page, dest_table)
-    self:getManga(self.Link.."/filterList?alpha="..search.."&sortBy=views&asc=false&page="..page, dest_table)
-end
-
-function LeoManga:getChapters(manga, dest_table)
-	local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = manga.Link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
+function LeoManga:getManga(link, dt)
+    local content = downloadContent(link)
+    dt.NoPages = true
+    for Link, ImageLink, Name in content:gmatch("<a href=\"([^\"]-)\">[^>]-src='([^']-)' alt='([^']-)'>[^<]-</a>") do
+        dt[#dt + 1] = CreateManga(stringify(Name), Link, ImageLink, self.ID, Link)
+        dt.NoPages = false
+        coroutine.yield(false)
     end
-    local content = file.string or ""
+end
+
+function LeoManga:getPopularManga(page, dt)
+    self:getManga(self.Link .. "/filterList?sortBy=views&asc=false&page=" .. page, dt)
+end
+
+function LeoManga:searchManga(search, page, dt)
+    self:getManga(self.Link .. "/filterList?alpha=" .. search .. "&sortBy=views&asc=false&page=" .. page, dt)
+end
+
+function LeoManga:getChapters(manga, dt)
+    local content = downloadContent(manga.Link)
     local t = {}
     for Link, Name in content:gmatch("fa fa%-eye\"></i>[^<]-<a href=\"([^\"]-)\">([^<]-)</a>") do
         t[#t + 1] = {
-			Name = Name,
-			Link = Link,
-			Pages = {},
-			Manga = manga
-		}
+            Name = stringify(Name),
+            Link = Link,
+            Pages = {},
+            Manga = manga
+        }
     end
-	for i = #t, 1, -1 do
-		dest_table[#dest_table + 1] = t[i]
-	end
-end
-
-function LeoManga:prepareChapter(chapter, dest_table)
-	local file = {}
-	Threads.insertTask(file, {
-		Type = "StringRequest",
-		Link = chapter.Link,
-		Table = file,
-		Index = "string"
-	})
-	while Threads.check(file) do
-		coroutine.yield(false)
-    end
-    local content = file.string or ""
-	local t = dest_table
-	for Link in content:gmatch("img%-responsive\"[^>]-data%-src=' ([^']-) '") do
-        t[#t + 1] = Link
-		Console.write("Got " .. t[#t])
+    for i = #t, 1, -1 do
+        dt[#dt + 1] = t[i]
     end
 end
 
-function LeoManga:loadChapterPage(link, dest_table)
-	dest_table.Link = link
+function LeoManga:prepareChapter(chapter, dt)
+    local content = downloadContent(chapter.Link)
+    for Link in content:gmatch("img%-responsive\"[^>]-data%-src=' ([^']-) '") do
+        dt[#dt + 1] = Link
+        Console.write("Got " .. dt[#dt])
+    end
+end
+
+function LeoManga:loadChapterPage(link, dt)
+    dt.Link = link
 end
