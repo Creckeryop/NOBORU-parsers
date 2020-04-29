f50db0a9a623e890e0a4b5e2b686309d835c707d
Creckeryop <didager@yandex.ru>
2020-04-29 21:56:25 +0500

    [HENTAISHARK] Refactoring Code + HTML entities

--- parsers/[DF]HentaiShark.lua
+++ parsers/[DF]HentaiShark.lua
@@ -1,78 +1,69 @@
-if u8c then
-    HentaiShark = Parser:new("Hentai Shark", "https://readhent.ai", "DIF", "HENSHARKDIF", 1)
-    
-    HentaiShark.NSFW = true
-    local function stringify(string)
-        return string:gsub("&#([^;]-);", function(a)
-            local number = tonumber("0" .. a) or tonumber(a)
-            return number and u8c(number) or "&#" .. a .. ";"
-        end)
-    end
-    
-    local function downloadContent(link)
-        local file = {}
-        Threads.insertTask(file, {
-            Type = "StringRequest",
-            Link = link,
-            Table = file,
-            Index = "string",
-            Header1 = "accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
-        })
-        while Threads.check(file) do
-            coroutine.yield(false)
-        end
-        return file.string or ""
-    end
-    
-    function HentaiShark:getManga(link, dest_table)
-        local content = downloadContent(link)
-        local t = dest_table
-        local done = true
-        for Link, ImageLink, Name, flag in content:gmatch('manga%-cover%-frontend">.-href="[^"]-/manga/([^"]-)".-src=\'([^\']-)\' alt=\'([^>]-)\'>[^<]-</div>.-flag%-([^"]-)"') do
-            if flag=="gb" then flag="en" end
-            local manga = CreateManga("["..flag:upper().."]"..stringify(Name), Link, ImageLink, self.ID, self.Link .. "/manga/" .. Link)
-            if manga then
-                t[#t + 1] = manga
-                done = false
-            end
-            coroutine.yield(false)
-        end
-        if done then
-            t.NoPages = true
-        end
-    end
+HentaiShark = Parser:new("Hentai Shark", "https://readhent.ai", "DIF", "HENSHARKDIF", 2)
 
-    function HentaiShark:getLatestManga(page, dest_table)
-        self:getManga(self.Link .. "/manga-list?page="..page.."&sortBy=created_at&asc=false",dest_table)
-    end
-    
-    function HentaiShark:getPopularManga(page, dest_table)
-        self:getManga(self.Link .. "/manga-list?fl=1&page=" .. page .. "&sortBy=views&asc=false",dest_table)
-    end
-    
-    function HentaiShark:searchManga(search, page, dest_table)
-        self:getManga(self.Link .. "/advanced-search?fl=1&params=name%3D"..search.."%26author%3D&page="..page, dest_table)
-    end
+HentaiShark.NSFW = true
 
-    function HentaiShark:getChapters(manga, dest_table)
-        dest_table[#dest_table+1] = {
-            Name = "Read chapter",
-            Link = manga.Link,
-            Pages = {},
-            Manga = manga
-        }
-    end
-    
-    function HentaiShark:prepareChapter(chapter, dest_table)
-        local content = downloadContent(self.Link .."/manga/".. chapter.Link .. "/1/1")
-        local t = dest_table
-        for Link in content:gmatch("data%-src=' (%S-) '") do
-            t[#t + 1] = Link
-            Console.write("Got " .. t[#t])
-        end
-    end
+local function stringify(string)
+    return string:gsub("&#([^;]-);", function(a)
+        local number = tonumber("0" .. a) or tonumber(a)
+        return number and u8c(number) or "&#" .. a .. ";"
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
+end
 
-    function HentaiShark:loadChapterPage(link, dest_table)
-        dest_table.Link = link
+local function downloadContent(link)
+    local f = {}
+    Threads.insertTask(f, {
+        Type = "StringRequest",
+        Link = link,
+        Table = f,
+        Index = "text",
+        Header1 = "accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+    })
+    while Threads.check(f) do
+        coroutine.yield(false)
     end
+    return f.text or ""
+end
+
+function HentaiShark:getManga(link, dt)
+    local content = downloadContent(link)
+    dt.NoPages = true
+    for Link, ImageLink, Name, flag in content:gmatch('manga%-cover%-frontend">.-href="[^"]-/manga/([^"]-)".-src=\'([^\']-)\' alt=\'([^>]-)\'>[^<]-</div>.-flag%-([^"]-)"') do
+        if flag == "gb" then flag = "en" end
+        dt[#dt + 1] = CreateManga("[" .. flag:upper() .. "] " .. stringify(Name), Link, ImageLink, self.ID, self.Link .. "/manga/" .. Link)
+        dt.NoPages = false
+        coroutine.yield(false)
+    end
+end
+
+function HentaiShark:getLatestManga(page, dt)
+    self:getManga(self.Link .. "/manga-list?page=" .. page .. "&sortBy=created_at&asc=false", dt)
+end
+
+function HentaiShark:getPopularManga(page, dt)
+    self:getManga(self.Link .. "/manga-list?fl=1&page=" .. page .. "&sortBy=views&asc=false", dt)
+end
+
+function HentaiShark:searchManga(search, page, dt)
+    self:getManga(self.Link .. "/advanced-search?fl=1&params=name%3D" .. search .. "%26author%3D&page=" .. page, dt)
+end
+
+function HentaiShark:getChapters(manga, dt)
+    dt[#dt + 1] = {
+        Name = "Read chapter",
+        Link = manga.Link,
+        Pages = {},
+        Manga = manga
+    }
+end
+
+function HentaiShark:prepareChapter(chapter, dt)
+    local content = downloadContent(self.Link .. "/manga/" .. chapter.Link .. "/1/1")
+    for Link in content:gmatch("data%-src=' (%S-) '") do
+        dt[#dt + 1] = Link
+        Console.write("Got " .. dt[#dt])
+    end
+end
+
+function HentaiShark:loadChapterPage(link, dt)
+    dt.Link = link
 end
