06eeee3789eb136d8a0b9ce36fd2e08ac2706b21
Creckeryop <didager@yandex.ru>
2020-04-29 14:36:15 +0500

    [MANGAOWL] Refactoring Code + HTML entities + Latest Tab

--- parsers/[EN]MangaOwl.lua
+++ parsers/[EN]MangaOwl.lua
@@ -1,70 +1,49 @@
-MangaOwl = Parser:new("MangaOwl", "https://mangaowl.com", "ENG", "MANGAOWLEN")
-
-local notify = false
+MangaOwl = Parser:new("MangaOwl", "https://mangaowl.net", "ENG", "MANGAOWLEN", 1)
 
 local function stringify(string)
-    if not u8c then
-        if not notify then
-            Notifications.push("Please update app, to see fixed titles")
-            notify = true
-        end
-        return string
-    end
     return string:gsub("&#([^;]-);", function(a)
         local number = tonumber("0" .. a) or tonumber(a)
         return number and u8c(number) or "&#" .. a .. ";"
-    end)
+    end):gsub("&(.-);", function(a) return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";" end)
 end
 
 local function downloadContent(link)
-    local file = {}
-    Threads.insertTask(file, {
+    local f = {}
+    Threads.insertTask(f, {
         Type = "StringRequest",
         Link = link,
-        Table = file,
-        Index = "string"
+        Table = f,
+        Index = "text"
     })
-    while Threads.check(file) do
+    while Threads.check(f) do
         coroutine.yield(false)
     end
-    return file.string or ""
+    return f.text or ""
 end
 
-function MangaOwl:getPopularManga(page, dest_table)
-    local content = downloadContent(self.Link.."/list/"..page)
-    local t = dest_table
-    local done = true
-    for Link, ImageLink, Name in content:gmatch('w3%-list%-img">.-href="(%S-)".-image="(%S-)".-<td>([^<]-)</td>') do
-        local Manga = CreateManga(stringify(Name), Link:match("single/(.-)$"), ImageLink:gsub("%%", "%%%%"), self.ID, Link)
-        if Manga then
-            t[#t + 1] = Manga
-        end
-        done = false
-        coroutine.yield(false)
-    end
-    if done then
-        t.NoPages = true
-    end
-end
-
-function MangaOwl:searchManga(search, page, dest_table)
-    local content = downloadContent(self.Link.."/search/"..search.."/"..page)
-    local t = dest_table
-    local done = true
+function MangaOwl:getManga(link, dt)
+    local content = downloadContent(link)
+    dt.NoPages = true
     for ImageLink, Link, Name in content:gmatch('comicView".-image="(%S-)".-agileits_w3layouts_mid_1_home">.-href="(%S-)".->([^<]-)</a>') do
-        local Manga = CreateManga(stringify(Name), Link:match("single/(.-)$"), ImageLink:gsub("%%", "%%%%"), self.ID, Link)
-        if Manga then
-            t[#t + 1] = Manga
-        end
-        done = false
+        dt[#dt + 1] = CreateManga(stringify(Name), Link:match("single/(.-)$"), ImageLink:gsub("%%", "%%%%"), self.ID, Link)
+        dt.NoPages = false
         coroutine.yield(false)
-    end
-    if done then
-        t.NoPages = true
     end
 end
 
-function MangaOwl:getChapters(manga, dest_table)
+function MangaOwl:getPopularManga(page, dt)
+    self:getManga(self.Link.."/popular/"..page, dt)
+end
+
+function MangaOwl:getLatestManga(page, dt)
+    self:getManga(self.Link.."/lastest/"..page, dt)
+end
+
+function MangaOwl:searchManga(search, page, dt)
+    self:getManga(self.Link.."/search/"..search.."/"..page, dt)
+end
+
+function MangaOwl:getChapters(manga, dt)
     local content = downloadContent(self.Link.."/single/"..manga.Link)
     local t = {}
     for Link, Name in content:gmatch('chapter_list">.-href="([^"]-)".-label>[\n%s]-(%S[^<]-)[\n%s]-</label>') do
@@ -76,19 +55,18 @@ function MangaOwl:getChapters(manga, dest_table)
 		}
     end
 	for i = #t, 1, -1 do
-		dest_table[#dest_table + 1] = t[i]
+		dt[#dt + 1] = t[i]
 	end
 end
 
-function MangaOwl:prepareChapter(chapter, dest_table)
+function MangaOwl:prepareChapter(chapter, dt)
     local content = downloadContent(chapter.Link)
-    local t = dest_table
     for Link in content:gmatch('owl%-lazy" data%-src="(%S-)"') do
-        t[#t + 1] = Link
-		Console.write("Got " .. t[#t])
+        dt[#dt + 1] = Link
+		Console.write("Got " .. dt[#dt])
     end
 end
 
-function MangaOwl:loadChapterPage(link, dest_table)
-	dest_table.Link = link
+function MangaOwl:loadChapterPage(link, dt)
+	dt.Link = link
 end
