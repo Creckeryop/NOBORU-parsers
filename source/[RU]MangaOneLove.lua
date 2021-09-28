056a6755bc1eda4f2eb00aaef5126e7a2aa250fd
Creckeryop <didager@yandex.ru>
2021-09-28 14:41:01 +0500

    [MANGAONELOVE] Fixed getting chapter

--- source/[RU]MangaOneLove.lua
+++ source/[RU]MangaOneLove.lua
@@ -1,4 +1,4 @@
-MangaOneLove = Parser:new("MangaOneLove", "https://mangaonelove.ru", "RUS", "MANGAONELOVERU", 2)
+MangaOneLove = Parser:new("MangaOneLove", "https://mangaonelove.ru", "RUS", "MANGAONELOVERU", 3)
 
 local function downloadContent(link)
 	local f = {}
@@ -28,15 +28,6 @@ local function stringify(string)
 		"&(.-);",
 		function(a)
 			return HTML_entities and HTML_entities[a] and u8c(HTML_entities[a]) or "&" .. a .. ";"
-		end
-	)
-end
-
-local function stringify2(string)
-	return string:gsub(
-		"\\u(....)",
-		function(a)
-			return u8c(tonumber("0x" .. a))
 		end
 	)
 end
@@ -76,15 +67,35 @@ end
 function MangaOneLove:getChapters(manga, dt)
 	local content = downloadContent(self.Link .. "/manga/" .. manga.Link)
 	local description = (content:match('class="summary__content.-p>(.-)</p>') or ""):gsub("<.->",""):gsub("\n+","\n"):gsub("^%s+",""):gsub("%s+$","")
+	local id = content:match('"manga_id":"(%d*)"')
+	local chapters_content = downloadContent({
+		Link = "https://mangaonelove.ru/wp-admin/admin-ajax.php",
+		HttpMethod = POST_METHOD,
+		PostData = "action=manga_get_chapters&manga="..id
+	})
 	dt.Description = stringify(description)
 	local t = {}
-	for Link, Name in content:gmatch('wp%-manga%-chapter%s*"> <a href="[^"]+/([^"]-)/">(.-)</a>') do
-		t[#t + 1] = {
-			Name = stringify(Name),
-			Link = Link,
-			Pages = {},
-			Manga = manga
-		}
+	local parted = false
+	for name, part in chapters_content:gmatch('>([^<]-)</a>%s*<ul class="sub%-chap list%-chap"(.-)</ul>') do
+		parted = true
+		for Link, Name in part:gmatch('wp%-manga%-chapter%s*">[^<]-<a href="[^"]+/([^"]-)/">%s*([^<]-)%s*</a>') do
+			t[#t + 1] = {
+				Name = stringify(name.." - ".. Name),
+				Link = Link,
+				Pages = {},
+				Manga = manga
+			}
+		end
+	end
+	if not parted then
+		for Link, Name in chapters_content:gmatch('wp%-manga%-chapter%s*">[^<]-<a href="[^"]+/([^"]-)/">%s*([^<]-)%s*</a>') do
+			t[#t + 1] = {
+				Name = stringify(Name),
+				Link = Link,
+				Pages = {},
+				Manga = manga
+			}
+		end
 	end
 	for i = 1, #t do
 		dt[#dt + 1] = t[i]
